391d551ea393645b6c7300b0e6cf1e615c3bf6b7
Kalle Møller <git@k-moeller.dk>
2016-10-10 13:50:07 +0200

    Opcodes start

---
+++ includes/LittleVirtualMachine/LittleInterpeter/Opcodes.h
@@ -0,0 +1,12 @@
+//
+// Created by Kalle Møller on 10/10/2016.
+//
+
+#ifndef LITTLEVIRTUALMACHINE_OPCODES_H
+#define LITTLEVIRTUALMACHINE_OPCODES_H
+
+#include <string>
+
+
+
+#endif //LITTLEVIRTUALMACHINE_OPCODES_H
---
+++ includes/LittleVirtualMachine/LittleShared/opcodes.h
@@ -0,0 +1,19 @@
+//
+// Created by Kalle Møller on 10/10/2016.
+//
+
+#ifndef LITTLEVIRTUALMACHINE_LITTLESHARED_OPCODES_H
+#define LITTLEVIRTUALMACHINE_LITTLESHARED_OPCODES_H
+
+class BaseOpcode {
+    static const uint8_t code;
+    static const std::string name;
+};
+
+class Add : BaseOpcode {
+    static const uint8_t code = 0x02;
+    static const std::string name = "ADD";
+};
+
+
+#endif //LITTLEVIRTUALMACHINE_LITTLESHARED_OPCODES_H
