e1a687dd7026bab1efde1415c32f8519c551ecbb
KasperSK <raizzat@gmail.com>
2016-10-19 15:16:00 +0200

    Curses

---
+++ src/LittleApplication/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by kaspersk on 10/19/16.
+//
+
+#include "LittleVirtualMachine/LittleApplication/LittleMenu.h"
+
+int main(){
+    TestApplication *Demo = new TestApplication();
+    Demo->run();
+
+    return 0;
+}
